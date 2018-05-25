0a3a2a005e62c615251e2aad57f3ace611909cfa
Donnie Karns <donniekarnsinsb@hotmail.com>
2018-05-25 10:39:15 -0700

    Create secrets config to allow for checking in terminal command scripts into github

--- scenegen.py
+++ scenegen.py
@@ -16,14 +16,14 @@ def get_states(url, key):
     headers = {'x-ha-access': key}
   else:
     headers = {}
-    
+
   apiurl = url + "/api/states"
-  
+
   r = requests.get(apiurl, headers=headers)
-  
+
   if r.status_code != 200:
     error("Error calling Home Assistant: {}, {}".format(r.status_code, r.reason))
-    
+
   return r.json()
 
 def output_attrs(state, args):
@@ -35,7 +35,7 @@ def output_attrs(state, args):
   if type == "light" and "light" in args.types:
     print ("  {}:".format(state["entity_id"]))
     print ("    state: {}".format(state["state"]))
-    for attr in light_attrs:      
+    for attr in light_attrs:
       if attr in state["attributes"]:
           print("    {}: {}".format(attr, round(float(state["attributes"][attr]))))
     for color_type in light_color_types:
@@ -44,38 +44,47 @@ def output_attrs(state, args):
   if type == "switch" and "switch" in args.types:
     print ("  {}:".format(state["entity_id"]))
     print ("    state: {}".format(state["state"]))
-  
-def main():
 
+def main():
   # Get command line args
-  
+
   parser = argparse.ArgumentParser()
 
-  parser.add_argument("url", help="url for Home Assistant instance")
+  parser.add_argument("-u", "--url", help="url for Home Assistant instance")
   parser.add_argument("-k", "--key", help="API Key of Home Assistant instance")
   parser.add_argument("-s", "--scenename", help="Name of scene to generate", default = "My New Scene")
   parser.add_argument("-m", "--mapfile", help="Name of mapfile to enable device filtering")
+  parser.add_argument("-e", "--secrets", help="Name of secrets config to hide sensitive data")
   parser.add_argument("-f", "--filter", help="Comma separated list of device collections as defined in mapfile")
   parser.add_argument("-c", "--colortype", help="color type to use", default = "color_temp", choices = light_color_types)
   parser.add_argument("-t", "--types", help="list of device types to include", default = "light,switch")
   args = parser.parse_args()
-  
+
+  if args.url is None and args.secrets is None:
+    error("You must specify either a url or secrets file")
+
   devices = {}
+  config = configparser.ConfigParser()
   if args.mapfile:
-    config = configparser.ConfigParser()
     config.read_file(open(args.mapfile))
     for section in config.sections():
       devices[section] = {}
       for option in config.options(section):
         devices[section][option] = config.get(section, option)
-  
+
   filters = []
   if args.filter:
     if args.mapfile:
       filters = args.filter.split(",")
     else:
       error("Must specify a mapfile if using filters")
+
   try:
+    if args.secrets:
+      config.read_file(open(args.secrets))
+      args.key = config.get("HA","api_key")
+      args.url = config.get("HA","ha_url")
+
     states = get_states(args.url, args.key)
     print("name: {}".format(args.scenename))
     print("entities:")
@@ -94,6 +103,6 @@ def main():
 
   except:
     raise
- 
+
 if __name__ == "__main__":
     main()
